29c87544bf156449151485a5ab2cf665e52d3364
student <azriel.zev@gmail.com>
2024-10-08 16:49:50 +0300

    I made a third screen and added a single question

--- app/src/main/java/com/example/asfirstapp/Second.java
+++ app/src/main/java/com/example/asfirstapp/Second.java
@@ -1,7 +1,10 @@
 package com.example.asfirstapp;
 
 import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
@@ -11,9 +14,10 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class Second extends AppCompatActivity {
+public class Second extends AppCompatActivity implements View.OnClickListener {
     TextView TV;
     EditText ET;
+    Button BtCLick1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,5 +33,14 @@ public class Second extends AppCompatActivity {
         TV = findViewById(R.id.TV);
         ET =  findViewById(R.id.ET);
         TV.setText("Ready " + getIntent().getStringExtra("name")+"?");
+        BtCLick1 = findViewById(R.id.BtClick1);
+        BtCLick1.setOnClickListener(this);
     }
+    @Override
+    public void onClick(View view){
+        Intent intent = new Intent(this, Third.class);
+        startActivity(intent);
+    }
+
+    ;
 }
---
+++ app/src/main/java/com/example/asfirstapp/Third.java
@@ -0,0 +1,28 @@
+package com.example.asfirstapp;
+
+import android.os.Bundle;
+import android.widget.Button;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class Third extends AppCompatActivity {
+    Button BtClick2;
+    Button BtClick3;
+    Button BtClick4;
+    Button BtClick5;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_third);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+}
