379eaa3e3092e4db219843753e08157307cb71ad
azrie <azriel.zev@gmail.com>
2024-10-15 14:06:12 +0300

    I added a losing screen and made a few buttons work

--- app/src/main/java/com/example/asfirstapp/Third.java
+++ app/src/main/java/com/example/asfirstapp/Third.java
@@ -1,7 +1,10 @@
 package com.example.asfirstapp;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,7 +12,8 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class Third extends AppCompatActivity {
+public class Third extends AppCompatActivity implements View.OnClickListener {
+    TextView TV1;
     Button BtClick2;
     Button BtClick3;
     Button BtClick4;
@@ -19,10 +23,26 @@ public class Third extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_third);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main2), (v, insets) -> {
+            initViews();
             return insets;
         });
+    }
+
+    private void initViews() {
+        TV1 = findViewById(R.id.TV1);
+        BtClick2 = findViewById(R.id.BtClick2);
+        BtClick3 = findViewById(R.id.BtClick3);
+        BtClick4 = findViewById(R.id.BtClick4);
+        BtClick5 = findViewById(R.id.BtClick5);
+        BtClick2.setOnClickListener(this);
+        BtClick4.setOnClickListener(this);
+        BtClick5.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        Intent intent =  new Intent(this, Failure.class);
+        startActivity(intent);
     }
 }
